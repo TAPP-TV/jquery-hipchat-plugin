c592e41724300caa586ba7f52cbc18729ce77c90
Robert Bergman <robert.bergman@gmail.com>
2013-08-09 15:44:41 -0600

    Light docs and support urls without protocols

--- jquery.hipchat.js
+++ jquery.hipchat.js
@@ -1,11 +1,14 @@
 (function ($) {
 
-  // url
-  // timezone
-  // welcome
-  // noframes
-  // width
-  // height
+  // Creates an iframe with an embedded HipChat conversation window.
+  //
+  // Options:
+  //   url        - The url to the room to embed; required
+  //   timezone   - The timezone to use in the embedded room; required
+  //   welcome    - A welcome message to display when the room is joined; optional
+  //   noframes   - Content to include when iframes are disabled in the browser; optional
+  //   width      - The width of the iframe; defaults to 100%
+  //   height     - The height of the iframe; defaults to 400px
   $.fn.hipchat = function (options) {
     if (options && options.url && options.timezone) {
       var params = {
@@ -17,7 +20,9 @@
         params.welcome_msg = options.welcome;
       }
       var url = options.url + (options.url.indexOf('?') > 0 ? '&' : '?') + $.param(params);
-      console.log(url);
+      if (url.indexOf('https://') !== 0) {
+        url = 'https://' + url;
+      }
       var w = options.width || '100%';
       var h = options.height || 400;
       var nf = (options.noframes || '');
